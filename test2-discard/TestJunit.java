164a4f357bf55e32cfe62b2ce2fec395dda3ec0d
collinsdevp <ogbonna.collins@gmail.com>
2024-04-28 13:27:41 +0100

    added junit test

---
+++ test2-discard/TestJunit.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestJunit {
+	
+  
+
+   @Test
+   public void testPrintMessage() {
+      assertEquals(0,0);
+   }
+}
